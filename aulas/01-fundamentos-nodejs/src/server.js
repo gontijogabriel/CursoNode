54c7b93d77f9ea8e7b4b41446b74e62b697a1de1
Gabriel Gontijo <gontijogabr@gmail.com>
2023-11-01 11:34:55 -0300

    criando middlewares

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,62 +1,15 @@
-// const a = 5;
-// const b = 7;
-
-// console.log(a + b)
-// OUTPUT node src/server.js
-// OUTPUT 12
-
-// CommonJS => require
-// const http = require('http')
-
-// ESModules => import/export
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
-// Rotas / Metodos HTTP e URL
-// - Criar
-// - Listar 
-// - Editar
-// - Deletar
-
-// GET, POST, PUT, PATCH, DELETE
-// GET = Buscar um recurso do backend (ex: lista de usuarios)
-// POST = Criar um recurso no backend (ex: usuario)
-// PUT = Atualizar um recurso por completo no backend
-// PATCH = Atualizar um recurso especifico de uma entidade no backend (ex: nome de usuario)
-// DELETE = Deletar um recurso do backend
-
-// Stateful - Stateless
-
-// JSON - JavaScript Object Notation
-
-// Cabeçalhos (Requisicao/Respostas) => Metadados
-
-// HTTP Status Code
 const users = []
 
 const server = http.createServer(async (request, response) => {
     const { method, url } = request
 
-    const buffers = []
-
-    for await (const chunk of request) {
-        buffers.push(chunk)
-    }
-
-    // const fullStreamContent = Buffer.concat(buffers).toString()
-
-    try {
-        request.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        request.body = null
-    }
-
-    console.log(request.body)
+    await json(request, response)
     
-    // console.log(method, url)
-    // GET /
     if (method == 'GET' && url == '/users') {
         return response
-            .setHeader('Content-type', 'application/json')
             .end(JSON.stringify(users))
     }
 
