88a2a097a7d31f3a0120e95a299ba023d6ebfe10
Gabriel Gontijo <gontijogabr@gmail.com>
2023-11-01 14:49:48 -0300

    feat: dabase.js + crud functions

--- aulas/01-fundamentos-nodejs/src/middlewares/routes.js
+++ aulas/01-fundamentos-nodejs/src/middlewares/routes.js
@@ -1,3 +1,4 @@
+import { buildRoutPath } from "../utils/build-route-path.js"
 import { Database } from "./database.js"
 import { randomUUID } from 'node:crypto'
 
@@ -6,7 +7,7 @@ const database = new Database()
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutPath('/users'),
         handler: (request, response) => {
             const users = database.select('users')
 
@@ -15,7 +16,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutPath('/users'),
         handler: (request, response) => {
             const { name, email } = request.body
 
@@ -30,4 +31,30 @@ export const routes = [
             return response.writeHead(201).end()
         }
     },
+    {
+        method: 'PUT',
+        path: buildRoutPath('/users/:id'),
+        handler: (request, response) => {
+            const { id } = request.params
+            const { name, email } = request.body
+
+            database.update('users', id, {
+                name,
+                email,
+            })
+
+            return response.writeHead(204).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutPath('/users/:id'),
+        handler: (request, response) => {
+            const { id } = request.params
+
+            database.delete('users', id)
+
+            return response.writeHead(204).end()
+        }
+    }
 ]
